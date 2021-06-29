7144ce4fdc561a7e142f22caff035a26bb89d7ce
bertrand.cerot <bertrand.cerot@cgi.com>
2021-06-29 14:25:06 +0200

    premier commit

---
+++ src/main/java/com/ipiecoles/communes/web/repository/CommuneRepository.java
@@ -0,0 +1,24 @@
+package com.ipiecoles.communes.web.repository;
+
+import com.ipiecoles.communes.web.model.Commune;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CommuneRepository extends JpaRepository<Commune, String> {
+
+    @Query("from Commune c")
+    List<Commune> findAllCommunesDB();
+
+    @Query("select count(distinct c.codePostal) from Commune c")
+    long countDistinctCodePostal();
+
+    @Query("select count(distinct c.nom) from Commune c")
+    long countDistinctAllCommunes();
+
+
+
+
+
+}
