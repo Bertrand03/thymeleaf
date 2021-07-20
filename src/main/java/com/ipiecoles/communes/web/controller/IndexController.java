96d8e590f9b8caf0b6aa235965ff9d435bdb78e5
bertrand.cerot <bertrand.cerot@cgi.com>
2021-07-20 20:41:23 +0200

    Partie "Affichage des communes x à y"

--- src/main/java/com/ipiecoles/communes/web/controller/IndexController.java
+++ src/main/java/com/ipiecoles/communes/web/controller/IndexController.java
@@ -11,9 +11,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.List;
 
 @Controller
 public class IndexController {
